5ff6c44cde846652da639081553f0777b7ab2fb2
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 22:28:45 +0000

    Voting Age Limit commit

---
+++ VotingAgeLimit.java
@@ -0,0 +1,20 @@
+package votingagelimit;
+
+
+
+public class VotingAgeLimit {
+
+    public static void main(String[] args)
+    {
+        int age=19;
+        if (age>=18)
+        {
+            System.out.println("Candidate is eligible for Voting");
+
+        } else
+
+        {
+            System.out.println("candidate is not eligible for voting");
+        }
+    }
+}
